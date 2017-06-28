064964b23f040da63d237b1941d3389ec5740268
jorgebastos <jorgepbastos8@gmail.com>
2017-06-28 02:40:25 +0100

    Add files via upload

---
+++ site/db-connect.php
@@ -0,0 +1,13 @@
+<?php
+# Fill our vars and run on cli
+# $ php -f db-connect-test.php
+
+$dbname = 'cinemaecidades';
+$dbuser = 'cinemaecidades';
+$dbpass = 'jorgepbastos8';
+$dbhost = 'cinemaecidades.dsi.uminho.pt';
+
+$link = mysqli_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost'");
+mysqli_select_db($link, $dbname) or die("Could not open the db '$dbname'");
+
+?>
